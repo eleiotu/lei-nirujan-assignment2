795c498747d58858e6b24fbb0899d38b3821b27f
nirujan-v <nirujan.v@hotmail.com>
2023-04-06 17:24:43 -0400

    initial commit

--- src/main/java/com/example/webchatserver/ChatServer.java
+++ src/main/java/com/example/webchatserver/ChatServer.java
@@ -7,6 +7,8 @@ import jakarta.websocket.server.ServerEndpoint;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -16,6 +18,7 @@ import java.io.IOException;
 public class ChatServer {
 
     // contains a static List of ChatRoom used to control the existing rooms and their users
+    private Map<String, String> usernames = new HashMap<String, String>();
 
     // you may add other attributes as you see fit
 
@@ -27,6 +30,7 @@ public class ChatServer {
         session.getBasicRemote().sendText("First sample message to the client");
 //        accessing the roomID parameter
         System.out.println(roomID);
+        session.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\" \\n Welcome to the chat room. Please state your name\"}");
 
 
 
@@ -34,21 +38,54 @@ public class ChatServer {
 
     @OnClose
     public void close(Session session) throws IOException, EncodeException {
+
         String userId = session.getId();
-        // do things for when the connection closes
+        if(usernames.containsKey(userId))
+        {
+            String username = usernames.get(userId);
+            usernames.remove(userId);
+            for( Session peer: session.getOpenSessions())
+            {
+                peer.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\" (Server): " + username + " left the chat room.\"}");
+            }
+        }
     }
 
     @OnMessage
     public void handleMessage(String comm, Session session) throws IOException, EncodeException {
 //        example getting unique userID that sent this message
         String userId = session.getId();
+        String userID = session.getId();
+        JSONObject jsonmsg = new JSONObject(comm);
+        String type = (String) jsonmsg.get("type");
+        String message = (String) jsonmsg.get("message");
 
-//        Example conversion of json messages from the client
-        //        JSONObject jsonmsg = new JSONObject(comm);
-//        String val1 = (String) jsonmsg.get("attribute1");
-//        String val2 = (String) jsonmsg.get("attribute2");
 
-        // handle the messages
+        //Check if user is part of users
+
+        if(usernames.containsKey(userID)){
+            String username = usernames.get(userID);
+            System.out.println(username);
+            //This is not their first message
+            for(Session peer: session.getOpenSessions()) {
+                peer.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\"(" + username + "): " + message + "\"}");
+            }
+
+        } else { //first message is their username
+            usernames.put(userID, message);
+            for(Session peer: session.getOpenSessions()){
+
+                if(!peer.getId().equals(userID)){
+                    peer.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\" (Server): " + message + " joined the chat room.\"}");
+                }else {
+                    peer.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\" (Server): Welcome, " + message + "\"}");
+                }
+
+
+            }
+        }
+
+
 
 
     }
